10d7934c50a7b71e48a6ba4c2d1ee58279a54758
carlosestefan <79671191+carlosestefan@users.noreply.github.com>
2021-08-23 18:30:14 -0300

    exercicio pronto

--- prog-imperativa-1/playground/aula05-ex.js
+++ prog-imperativa-1/playground/aula05-ex.js
@@ -27,19 +27,58 @@ function idadeCao (idade){
 
 console.log(idadeCao(4));
 
-// 5. Valor/Hora de trabalho - OK
+//5. Valor/Hora de trabalho - OK
 function valorHora (salario){
     return salario / 160
 }
 
 console.log(valorHora(6000));
 
-// IMC - OK
+//6. IMC - OK
 function imc (peso, altura){
     return peso / (altura**2)
 }
 
 console.log(imc(90, 1.80));
+
+//7. Calculadora - Nível 1
+function somar(a, b){
+    return a + b
+}
+
+function subtrair(a, b){
+    return a - b
+}
+
+function multiplicar(a, b){
+    return a * b
+}
+
+function dividir(a, b){
+    return a / b
+}
+
+function quadrado(a){
+    return (multiplicar(a, a))
+}
+
+function media (a, b, c){
+    return (a + b + c) / 3
+}
+
+function porcentagem (a, b){
+    return a * (b/100)
+}
+
+console.log(somar(10, 1));
+
+console.log(dividir(10, 0));
+
+console.log(quadrado(3));
+
+console.log(media(7, 10, 5));
+
+console.log(porcentagem(40, 50))
 
 
 
