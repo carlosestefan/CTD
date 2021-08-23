ad9163378993c477942c834fd987e304c3a65d41
carlosestefan <79671191+carlosestefan@users.noreply.github.com>
2021-08-23 13:43:21 -0300

    playground tipos de dados

--- prog-imperativa-1/playground/tipos_dados.js
+++ prog-imperativa-1/playground/tipos_dados.js
@@ -1,5 +1,51 @@
-var nome = Carlos
+// var se usa no código todo
+var nome = "Carlos";
 
+// let é utilizado dentro de um bloco de código (entre chaves)
 if(true){
-    let nome = Daniela
+    let nome = "Daniela";
 }
+
+// const é um dado fixo, que não pode ser alterado depois
+const estadoCivil = "casados";
+
+// string é um texto, e é declarado entre aspas (duplas ou simples)
+
+// inteiro
+let idade = 36;
+
+// decimal
+let altura = 1.83;
+
+//boolean
+let diaSeco = true;
+let diaUmido = false;
+
+// não é um número (Not a Number)
+let naoNumerico = NaN;
+
+// dados nulos
+let passageirosTrem = null;
+
+// indefinidos
+let passageirosOnibus = undefined;
+passageirosOnibus = 40
+
+// objeto literal
+let meuCarro = {
+    marca : "fiat",
+    anoFabricacao : 2021
+}
+
+// Arrays (lista) - Contagem começa SEMPRE DO ZERO - A ordem representa a ordem da lista, abaixo 0, 1, 2, 3
+let seriesFavoritas = ["Friends", "Breaking Bad", "Chaves", "The Office"];
+
+console.log(seriesFavoritas);
+
+
+
+
+
+
+
+
