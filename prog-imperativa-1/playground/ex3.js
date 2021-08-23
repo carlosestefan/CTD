2b9663aa093ccc2fe218270b0be22c3bf86fbf00
carlosestefan <79671191+carlosestefan@users.noreply.github.com>
2021-08-23 15:30:47 -0300

    exercícios 3 e 4

---
+++ prog-imperativa-1/playground/ex3.js
@@ -0,0 +1,8 @@
+let x = 3;
+let y = 2;
+
+let soma = x + y;
+
+let a = 1
+
+console.log(soma + a);
---
+++ prog-imperativa-1/playground/ex4.js
@@ -0,0 +1,11 @@
+let nome = "Carlos Estefan";
+let idade = 36;
+let peso = 90;
+let altura = 1.83;
+const plano = true;
+
+let imc = peso/(altura**2);
+
+console.log(`Olá, ${nome}, seu índice de massa corporal é ${Math.round(imc)}`);
+
+
