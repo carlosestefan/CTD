65aaf488fe300d645b88600e9997d24c1beb75db
carlosestefan <79671191+carlosestefan@users.noreply.github.com>
2021-08-25 17:45:01 -0300

    testes exercícios

--- prog-imperativa-1/aula-07/exercicio.js
+++ prog-imperativa-1/aula-07/exercicio.js
@@ -1,26 +1,94 @@
+var pessoa = {
+    nome: '',
+    peso : 0,
+    altura : 0,
+    imc : 0,
+    classificacao: ''
+};
+
+/*
+function consulta(){
+    var r = confirm(`Sua classificação é ${pessoa.classificacao}. Você gostaria de marcar uma consulta com uma nutricionista?`)
+    var data;
+    if (r==true) {
+        pessoa.nome = prompt('Informe seu nome')
+        data = get
+        alert(`Olá, ${pessoa.nome}! Sua consulta foi marcada para ${data}, às ${hora}.`)
+    } else {
+       alert('Obrigado pela sua visita!')
+    }
+}
+*/
+
 function calcularImc() {
 
-    var pessoa = {
-        nome: prompt('Qual é o seu nome?'),
-        peso : parseFloat (prompt('Qual seu peso?')),
-        altura : parseFloat (prompt('Qual sua Altura?')),
-        imc : peso / (altura * altura),
-        classificacao: ''
-    }
+    pessoa.nome = prompt('Qual é o seu nome?');
+    pessoa.peso = parseFloat(prompt('Qual seu peso?'));
+    pessoa.altura = parseFloat(prompt('Qual sua Altura?'));
+    pessoa.imc = pessoa.peso / (pessoa.altura * pessoa.altura);
 
-   if (pessoa.imc < 18.5){
-   pessoa.classificacao = 'Magreza';
-    }else if (18.5 <= pessoa.imc <= 24.9){
-        pessoa.classificacao = 'Normal';
-   }else if (25 <= pessoa.imc <= 29.9){
-    pessoa.classificacao = 'Sobrepeso';
-    alert(`Olá, ${pessoa.nome}, seu IMC é ${pessoa.imc}, e sua classificação é de ${pessoa.classificacao}. Você precisa marcar uma consulta.`);
-   }else if (30 <= pessoa.imc <= 30.9){
-    pessoa.classificacao = 'Obesidade';
-    alert(`teste`);
-   }else {
-    pessoa.classificacao = 'Obesidade Grave';
-    alert(`teste`);
-}
+    if (pessoa.imc < 18.5){
+        pessoa.classificacao = 'Magreza'
+        alert(`Olá, ${pessoa.nome}! Seu IMC é ${pessoa.imc.toFixed(2)} e sua classificação é ${pessoa.classificacao}.`);
+    }else if (18.5 <= pessoa.imc && pessoa.imc <= 24.9){
+        pessoa.classificacao = 'Normal'
+        alert(`Olá, ${pessoa.nome}! Seu IMC é ${pessoa.imc.toFixed(2)} e sua classificação é ${pessoa.classificacao}.`);
+    }else if (25 <= pessoa.imc && pessoa.imc <= 29.9){
+        pessoa.classificacao = 'Sobrepeso'
+        alert(`Olá, ${pessoa.nome}! Seu IMC é ${pessoa.imc.toFixed(2)} e sua classificação é ${pessoa.classificacao}. Você precisa marcar uma consulta com a nutricionista Dra. Luana Nagydai.`);
+    }else if (30 <= pessoa.imc && pessoa.imc <= 39.9){
+        pessoa.classificacao = 'Obesidade'
+        alert(`Olá, ${pessoa.nome}! Seu IMC é ${pessoa.imc.toFixed(2)} e sua classificação é ${pessoa.classificacao}. Você precisa marcar uma consulta com a nutricionista Dra. Luana Nagydai.`);
+    }else if(pessoa.imc >= 40){
+        pessoa.classificacao = 'Obesidade Grave'
+        alert(`Olá, ${pessoa.nome}! Seu IMC é ${pessoa.imc.toFixed(2)} e sua classificação é ${pessoa.classificacao}. Você precisa marcar uma consulta com a nutricionista Dra. Luana Nagydai.`);
+    };
 
-}
+};
+
+
+/*
+            Nível I - Refatorando
+            O que pode ser feito para melhorar a legibilidade¹ do código?
+
+            ¹Facilidade de entendimento do algoritimo gerado no código. 
+
+            Nível II - Adicionando novas funcionalidades
+            O usuário com sobrepeso, obsidade e/ou obsidade grave deverão:
+            - Receber uma alerta recomendando uma consulta com nutricionista (Boleano - true ou false);
+                - Caso a resposta seja sim, preencherá com:
+                    - Seu nome;
+                    - Dia da semana que pretende se consultar;
+                    - O horário do dia que pretende se consultar;
+                    - Agradecer o usuário pelo nome e informar o dia e horário da consulta com o Dra. Luana Nagydai.
+                - Caso a resposta seja não:
+                    - Receberá um pedido de agradecimento.
+
+            Nível III - Correção de error 
+            (🍒 do bolo)
+            Recebemos um pedido de alteração da clínica:
+            O usuário com sobrepeso, obsidade e/ou obsidade grave deverão:
+            - Receber uma alerta recomendando uma consulta com nutricionista (Boleano - true ou false);
+                - Caso a resposta seja sim, preencherá com:
+                    - Seu nome;
+                    - Dia da semana que pretende se consultar;
+                    - O horário do dia que pretende se consultar;
+                    (ajuste)
+                    - Confirmar com o usuário se o dia e o horário estão corretos:
+                        Caso a resposta seja sim:
+                        - Segue o fluxo normalmente.
+                    - Caso a resposta seja não:
+                        - Dia da semana que pretende se consultar;
+                        - O horário do dia que pretende se consultar;
+                    (fim do ajuste)
+                    - Agradecer o usuário pelo nome e informar o dia e horário da consulta com o Dra. Luana Nagydai.
+                - Caso a resposta seja não:
+                    - Receberá um pedido de agradecimento.
+
+        */    
+
+        // luciano batista
+        // mariana dias arantes
+        // bianca moraes
+        // paulo souza
+        // carlos estefan
