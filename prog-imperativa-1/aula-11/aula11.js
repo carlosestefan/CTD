f56a9de6e867256cdce76b6dd2420f885bc2cbde
carlosestefan <79671191+carlosestefan@users.noreply.github.com>
2021-09-08 21:47:26 -0300

    aula 11 prog

---
+++ prog-imperativa-1/aula-11/aula11.js
@@ -0,0 +1,25 @@
+//REPETIÇÕES
+
+for(var valorInicial = 0; valorInicial <= 5; valorInicial++) {
+    //console.log(valorInicial);
+}
+
+var numero = 5;
+
+while (numero < 5) {
+    console.log(numero);
+    numero++;
+}
+
+var numeroInicial = 6;
+
+do {
+    console.log(numeroInicial)
+    numeroInicial++
+
+} while(numeroInicial < 5){
+    
+}
+
+
+
