33b90d34070bbbb95a99795fe8a69ebcb130cc82
carlosestefan <79671191+carlosestefan@users.noreply.github.com>
2021-09-22 22:27:26 -0300

    aula 17 prog final

--- prog-imperativa-1/aula-17/aula17.js
+++ prog-imperativa-1/aula-17/aula17.js
@@ -1,1 +1,13 @@
-// EXPRESS
+const express = require('express');
+
+const app = new express();
+
+app.get('/', function (requisicaoAoServidor, respostaDoServidor) {
+    respostaDoServidor.send('Seja bem-vindo(a) à Aula 17 de Programação Imperativa I');
+})
+
+app.get('/sobre', function (requisicaoAoServidor, respostaDoServidor) {
+    respostaDoServidor.send('Sobre o projeto');
+})
+
+app.listen(1234);
