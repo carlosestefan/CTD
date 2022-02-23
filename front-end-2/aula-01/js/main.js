5e9df818234ddd933dc0753194c078014d4dbb55
carlosestefan <79671191+carlosestefan@users.noreply.github.com>
2022-02-23 19:17:56 -0300

    front aula 2

---
+++ front-end-2/aula-01/js/main.js
@@ -0,0 +1,17 @@
+const numbers = [9, 15, 24, 32, 70]
+
+let result = 0
+for(number of numbers) {
+  result = number + result;
+  console.log(result)
+}
+
+/* 
+GRUPO 2
+Maiara Martinelli
+Salomão Santos
+Louise Nascimento
+Kevin Jonathan
+Rodrigo Silva Araújo
+Carlos Estefan
+*/
---
+++ front-end-2/aula-02/js/main.js
@@ -0,0 +1,48 @@
+let userChoices = parseInt(prompt('1 - Papel, 2 - Tesoura, 3 - Pedra'));
+let botChoice = parseInt(Math.random()*3+1)
+
+// if(userChoices === 1) {
+
+//   if(botChoice === 1) {
+//     alert('Você escolheu Papel e o Bot escolheu Papel: Empate!')
+//   };
+//   if(botChoice === 2) {
+//     alert('Você escolheu Papel e o Bot escolheu Tesoura: Você perdeu...')
+//   };
+//   if(botChoice === 3) {
+//     alert('Você escolheu Papel e o Bot escolheu Pedra: Você ganhou!')
+//   }
+
+// }
+
+// if(userChoices === 2) {
+
+//   if(botChoice === 1) {
+//     alert('Você escolheu Tesoura e o Bot escolheu Papel: Você ganhou!')
+//   };
+//   if(botChoice === 2) {
+//     alert('Você escolheu Tesoura e o Bot escolheu Tesoura: Empate!')
+//   };
+//   if(botChoice === 3) {
+//     alert('Você escolheu Tesoura e o Bot escolheu Pedra: Você perdeu...')
+//   }
+
+// }
+
+// if(userChoices === 3) {
+
+//   if(botChoice === 1) {
+//     alert('Você escolheu Pedra e o Bot escolheu Papel: Você perdeu...')
+//   };
+//   if(botChoice === 2) {
+//     alert('Você escolheu Pedra e o Bot escolheu Tesoura: Você ganhou!')
+//   };
+//   if(botChoice === 3) {
+//     alert('Você escolheu Pedra e o Bot escolheu Pedra: Empate!')
+//   }
+
+// }
+
+
+console.log(userChoices)
+console.log(botChoice)
